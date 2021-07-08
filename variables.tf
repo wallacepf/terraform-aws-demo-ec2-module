05fb22b54942a718ee4161771b35b706967bc35f
wallacepf <wallacepf@gmail.com>
2021-07-08 12:43:33 -0300

    Changed the sec ip reference

--- variables.tf
+++ variables.tf
@@ -1,29 +1,15 @@
+variable "inst_size" {}
+variable "inst_name" {}
+variable "inst_key_name" {}
+variable "inst_sec_group_id" {}
+variable "inst_subnet_id" {}
+
 variable "inst_ami" {
     default = "ami-03d5c68bab01f3496"
 }
 
-variable "inst_name" {
-  
-}
-
 variable "inst_count" {
   default = "1"
-}
-
-variable "inst_size" {
-  
-}
-
-variable "inst_key_name" {
-  
-}
-
-variable "inst_sec_group_id" {
-  
-}
-
-variable "inst_subnet_id" {
-  
 }
 
 variable "inst_common_tags" {
